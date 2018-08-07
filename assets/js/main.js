b8c549879b6f1d0db8f2e68b09b4b990a39cb832
Chengdong Zhang <lmajzcd@sina.com>
2018-08-07 11:51:40 +0800

    add project
    add project

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+requirejs.config({
+	baseUrl: "assets/js",
+	waitSeconds: 15,
+	paths: {
+		"jquery": "jquery/jquery-3.3.1.min",
+		"vue": "vue/vue.min",
+		"vuerouter": "vue/vue-router.min",
+		"httpVueLoader": "vue/httpVueLoader.min",
+		"toast": "toast/toast",
+	},
+	shim: {
+		"jquery": {
+			deps: []
+		},
+		"vue": {
+			exports: "vue",
+			deps: []
+		},
+		"vuerouter": {
+			exports: "VueRouter",
+			deps: ['vue']
+		},
+		"httpVueLoader": {
+			exports: "httpVueLoader",
+			deps: []
+		},
+		"toast": {
+			exports: "toast",
+			deps: ['jquery']
+		}
+	}
+});
+
+//项目启动文件
+requirejs(["vue", "vuerouter", "httpVueLoader", "jquery","toast"], function(Vue, VueRouter, httpVueLoader, jquery,toast) {
+
+	console.log("VUE : " + Vue.version);
+	window.toast = toast;
+	Vue.config.silent = true;
+	Vue.config.performance = true;
+	Vue.config.warnHandler = function(msg, vm, trace) {}
+	Vue.config.errorHandler = function(err, vm, info) {}
+
+	Vue.use(VueRouter);
+	Vue.use(httpVueLoader);
+
+	//TODO Router
+	var routes = [{
+		path: '/',
+		component: httpVueLoader('view/app.vue'),
+		meta: {}
+	}];
+
+	var router = new VueRouter({
+		routes: routes
+	});
+
+	var vm = new Vue({
+		router: router,
+		currentRoute: window.location.pathname
+	}).$mount('#mainContainer');
+});
+
---
+++ view/app.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="component_view">
+		<div class="flex_vertical">
+			<div class="flex_horizontal"><a >2018-08-06 10:11:50&nbsp;&nbsp;&nbsp;&nbsp;XXXX&nbsp;&nbsp;&nbsp;&nbsp;将某分支合并进来&nbsp;&nbsp;&nbsp;&nbsp;编译成功&nbsp;&nbsp;&nbsp;&nbsp;</a><button id="getBinRunAutoTest" @click="compile">部署</button></div>
+			<div class="flex_horizontal"><a >2018-08-06 10:11:50&nbsp;&nbsp;&nbsp;&nbsp;XXXX&nbsp;&nbsp;&nbsp;&nbsp;将某分支合并进来&nbsp;&nbsp;&nbsp;&nbsp;编译成功&nbsp;&nbsp;&nbsp;&nbsp;</a><button id="getBinRunAutoTest" @click="compile">部署</button></div>
+			<div class="flex_horizontal"><a >2018-08-06 10:11:50&nbsp;&nbsp;&nbsp;&nbsp;XXXX&nbsp;&nbsp;&nbsp;&nbsp;将某分支合并进来&nbsp;&nbsp;&nbsp;&nbsp;编译成功&nbsp;&nbsp;&nbsp;&nbsp;</a><button id="getBinRunAutoTest" @click="compile">部署</button></div>
+		</div>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		name: "app",
+		data: function() {
+			return {}
+		},
+		created: function() {
+		},
+		mounted: function() {
+		},
+		methods: {
+			compile: function() {
+				
+			},
+			testPass: function() {
+			
+			},
+			testFail: function() {
+				
+			},
+		}
+	}
+</script>
+
+<style scoped="scoped">
+
+</style>
